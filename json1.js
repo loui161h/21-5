2dccb9a6c517459b46ae6870bcc035b6d001d267
Rikke Gaardsøe <rikkegaardsoe@rikkes-air.home>
2020-04-16 14:14:49 +0200

    process2

--- json1.js
+++ json1.js
@@ -1,5 +1,5 @@
 const url1 = "http://louisesettrup.dk/kea/09_cms/enogtyvefem_wp/wordpress/wp-json/wp/v2/proces/88";
-const url2 = "http://louisesettrup.dk/kea/09_cms/enogtyvefem_wp/wordpress/wp-json/wp/v2/proces/88";
+
 
 let process;
 
@@ -24,4 +24,12 @@ async function hentData() {
 function visData() {
     document.querySelector("#overskrift1").textContent = process.title.rendered;
     document.querySelector("#tekst1").innerHTML = process.content.rendered;
+
+    document.querySelector("#underoverskrift").textContent = process.overskrift1;
+    document.querySelector("#tekst2").innerHTML = process.tekst1;
+
+    document.querySelector("#underoverskrift1").textContent = process.overskrift2;
+    document.querySelector("#tekst3").innerHTML = process.tekst2;
+
+
 }
