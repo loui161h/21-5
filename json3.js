d9e3b422f9cfbf47ad53221b6b71c19f0a6853ef
Pauline Hornbøll Nielsen <Pauline@paulines-macbook-air.home>
2020-04-16 18:03:04 +0200

    json 3 med koncept rettet virker

--- json3.js
+++ json3.js
@@ -1,26 +1,13 @@
 // 21-5 KONCEPTET ******************************** 21-5 KONCEPTET  ****************************** 21-5 KONCEPTET  ********************************* //
 
 // Endpoint af vores json fil
-const endpoint6 = "http://louisesettrup.dk/kea/09_cms/enogtyvefem_wp/wordpress/wp-json/wp/v2/koncept/83";
-const endpoint7 = "http://louisesettrup.dk/kea/09_cms/enogtyvefem_wp/wordpress/wp-json/wp/v2/koncept/83";
-const endpoint8 = "http://louisesettrup.dk/kea/09_cms/enogtyvefem_wp/wordpress/wp-json/wp/v2/koncept/83";
-const endpoint9 = "http://louisesettrup.dk/kea/09_cms/enogtyvefem_wp/wordpress/wp-json/wp/v2/koncept/83";
-const endpoint10 = "http://louisesettrup.dk/kea/09_cms/enogtyvefem_wp/wordpress/wp-json/wp/v2/koncept/83";
-const endpoint11 = "http://louisesettrup.dk/kea/09_cms/enogtyvefem_wp/wordpress/wp-json/wp/v2/koncept/83";
-const endpoint12 = "http://louisesettrup.dk/kea/09_cms/enogtyvefem_wp/wordpress/wp-json/wp/v2/koncept/83";
-const endpoint13 = "http://louisesettrup.dk/kea/09_cms/enogtyvefem_wp/wordpress/wp-json/wp/v2/koncept/83";
-
-// definerer en variabel vi kalder alleTekster
-let heading1;
-let tekst1;
-let overskrift;
-let tekst;
-let overskrift_2;
-let tekst2;
-let overskrift_3;
-let tekst3;
+const url1 = "http://louisesettrup.dk/kea/09_cms/enogtyvefem_wp/wordpress/wp-json/wp/v2/koncept/83";
 
 
+let koncept;
+let hvad;
+let hvor;
+let hvorfor;
 
 
 
@@ -36,26 +23,25 @@ function start() {
 
 
 async function hentData() {
-    const response6 = await fetch(endpoint6);
-    const response7 = await fetch(endpoint7);
-    const response8 = await fetch(endpoint8);
-    const response9 = await fetch(endpoint9);
-    const response10 = await fetch(endpoint10);
-    const response11 = await fetch(endpoint11);
-    const response12 = await fetch(endpoint10);
-    const response13 = await fetch(endpoint11);
+    const svar1 = await fetch(url1);
+    const svar2 = await fetch(url1);
+    const svar3 = await fetch(url1);
+    const svar4 = await fetch(url1);
 
 
 
 
-    heading1 = await response6.json();
-    tekst1 = await response7.json();
-    overskrift = await response8.json();
-    tekst = await response9.json();
-    overskrift_2 = await response10.json();
-    tekst2 = await response11.json();
-    overskrift_3 = await response12.json();
-    tekst3 = await response13.json()
+
+
+
+
+    koncept = await svar1.json();
+    hvad = await svar2.json();
+    hvor = await svar3.json();
+    hvorfor = await svar4.json();
+
+
+
 
     visData();
 }
@@ -63,15 +49,22 @@ async function hentData() {
 
 function visData() {
     //document.querySelector("#koncept_img").src = koncept.billede.guid;
-    document.querySelector("#konceptet_heading1").textContent = heading1.title.rendered;
-    document.querySelector("#konceptet_tekst1").innerHTML = tekst1.content.rendered;
-    document.querySelector("#hvad_overskrift").textContent = overskrift.overskrift;
-    document.querySelector("#hvad_tekst").innerHTML = tekst.tekst;
-    document.querySelector("#hvor_overskrift_2").textContent = overskrift_2.overskrift_2;
-    document.querySelector("#hvor_tekst2").innerHTML = tekst2.tekst2;
-    document.querySelector("#hvorfor_overskrift_3").textContent = overskrift_3.overskrift_3;
-    document.querySelector("#hvorfor_tekst3").innerHTML = tekst3.tekst3;
+
+    /*    21-5 konceptet*/
+    document.querySelector(".overskrift").textContent = koncept.title.rendered;
+    document.querySelector(".tekst").innerHTML = koncept.content.rendered;
+    document.querySelector(".underoverskrift1").textContent = koncept.overskrift;
+    document.querySelector(".tekst1").innerHTML = koncept.tekst;
+    document.querySelector(".underoverskrift2").textContent = koncept.overskrift_2;
+    document.querySelector(".tekst2").innerHTML = koncept.tekst2;
+
+    document.querySelector(".underoverskrift3").textContent = koncept.overskrift_3;
+    document.querySelector(".tekst3").innerHTML = koncept.tekst3;
+
 
 }
+
+
+
 
 // 21-5 KONCEPTET SLUT *********************** 21-5  KONCEPTET SLUT ************************** 21-5 KONCEPTET SLUT ********************************* //
