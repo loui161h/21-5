91f4b066fec6efd3f5611d4ccd86bbe749f38442
Pauline Hornbøll Nielsen <Pauline@paulines-macbook-air.home>
2020-04-16 18:22:55 +0200

    21-5 konceptet json

--- json3.js
+++ json3.js
@@ -2,12 +2,15 @@
 
 // Endpoint af vores json fil
 const url1 = "http://louisesettrup.dk/kea/09_cms/enogtyvefem_wp/wordpress/wp-json/wp/v2/koncept/83";
+const url2 = "http://louisesettrup.dk/kea/09_cms/enogtyvefem_wp/wordpress/wp-json/wp/v2/koncept/93";
 
 
 let koncept;
 let hvad;
 let hvor;
 let hvorfor;
+
+let mening;
 
 
 
@@ -24,11 +27,7 @@ function start() {
 
 async function hentData() {
     const svar1 = await fetch(url1);
-    const svar2 = await fetch(url1);
-    const svar3 = await fetch(url1);
-    const svar4 = await fetch(url1);
-
-
+    const svar2 = await fetch(url2);
 
 
 
@@ -36,11 +35,6 @@ async function hentData() {
 
 
     koncept = await svar1.json();
-    hvad = await svar2.json();
-    hvor = await svar3.json();
-    hvorfor = await svar4.json();
-
-
 
 
     visData();
@@ -57,10 +51,13 @@ function visData() {
     document.querySelector(".tekst1").innerHTML = koncept.tekst;
     document.querySelector(".underoverskrift2").textContent = koncept.overskrift_2;
     document.querySelector(".tekst2").innerHTML = koncept.tekst2;
-
     document.querySelector(".underoverskrift3").textContent = koncept.overskrift_3;
     document.querySelector(".tekst3").innerHTML = koncept.tekst3;
 
+
+    /*    Fordi det giver mening*/
+    //    document.querySelector(".mening_overskrift1").textContent = mening.title.rendered;
+    //    document.querySelector(".mening_tekst1").innerHTML = mening.content.rendered;
 
 }
 
