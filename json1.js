3e8fffa87377ef655aee718b32acc9647a5565fc
Rikke Gaardsøe <rikkegaardsoe@rikkes-air.home>
2020-04-16 15:30:07 +0200

    destination indhold og dk family indhold

--- json1.js
+++ json1.js
@@ -1,9 +1,11 @@
 const url1 = "http://louisesettrup.dk/kea/09_cms/enogtyvefem_wp/wordpress/wp-json/wp/v2/proces/88";
 const url2 = "http://louisesettrup.dk/kea/09_cms/enogtyvefem_wp/wordpress/wp-json/wp/v2/proces/106";
+const url3 = "http://louisesettrup.dk/kea/09_cms/enogtyvefem_wp/wordpress/wp-json/wp/v2/proces/107";
 
 
 let process;
 let skaberHjem;
+let stiftedeForeninger;
 
 // venter på content er loaded og kalder funktionen start
 document.addEventListener("DOMContentLoaded", start);
@@ -17,9 +19,11 @@ function start() {
 async function hentData() {
     const svar1 = await fetch(url1);
     const svar2 = await fetch(url2);
+    const svar3 = await fetch(url3);
 
     process = await svar1.json();
     skaberHjem = await svar2.json();
+    stiftedeForeninger = await svar3.json();
 
 
     visData();
@@ -47,6 +51,16 @@ function visData() {
 
     document.querySelector(".underoverskrift2").textContent = skaberHjem.overskrift2;
     document.querySelector(".tekst3").innerHTML = skaberHjem.tekst2;
+
+    /*    stiftede foreninger*/
+    document.querySelector(".overskrift1").textContent = stiftedeForeninger.title.rendered;
+    document.querySelector(".tekst1").innerHTML = stiftedeForeninger.content.rendered;
+
+    document.querySelector(".underoverskrift1").textContent = stiftedeForeninger.overskrift1;
+    document.querySelector(".tekst2").innerHTML = stiftedeForeninger.tekst1;
+
+    document.querySelector(".underoverskrift2").textContent = stiftedeForeninger.overskrift2;
+    document.querySelector(".tekst3").innerHTML = stiftedeForeninger.tekst2;
 
 
 
