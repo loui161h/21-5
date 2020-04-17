d93ffb29b91c4a9c76c076213cdac402f9800d93
loui161h <55078332+loui161h@users.noreply.github.com>
2020-04-17 16:23:24 +0200

    koncept json

--- koncept_json.js
+++ koncept_json.js
@@ -2,4 +2,51 @@ document.addEventListener("DOMContentLoaded", start);
 
 let urlParameter = new URLSearchParams(window.location.search);
 
-let tekstId;
+let tekstId = urlParameter.get("id");
+
+const startId = 271;
+
+
+let urlId;
+
+if (tekstId) {
+    urlId = tekstId;
+} else {
+    urlId = startId;
+}
+
+const url = "http://louisesettrup.dk/kea/09_cms/enogtyvefem_wp/wordpress/wp-json/wp/v2/tekst/" + urlId;
+
+
+let alleTekster;
+
+
+
+function start() {
+    hentData();
+
+}
+
+async function hentData() {
+    const response = await fetch(url);
+    alleTekster = await response.json();
+
+
+
+    visDataKoncept();
+}
+
+function visDataKoncept() {
+    /*    document.querySelector(".splash_img").src = alleTekster.billede1.guid;*/
+    document.querySelector("#koncept_overskrift1").textContent = alleTekster.title.rendered;
+    document.querySelector("#koncept_p1").innerHTML = alleTekster.content.rendered;
+
+    document.querySelector("#koncept_overskrift2").textContent = alleTekster.overskrift1;
+    document.querySelector("#koncept_p2").innerHTML = alleTekster.tekst1;
+
+    document.querySelector("#koncept_overskrift3").textContent = alleTekster.overskrift2;
+    document.querySelector("#koncept_p3").innerHTML = alleTekster.tekst3;
+
+    document.querySelector("#koncept_overskrift4").textContent = alleTekster.overskrift3;
+    document.querySelector("#koncept_p4").innerHTML = alleTekster.tekst5;
+}
