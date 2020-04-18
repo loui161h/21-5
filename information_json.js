e7df74d69f26b894a3d70058263d5856ad0207e0
Pauline Hornbøll Nielsen <Pauline@paulines-macbook-air.home>
2020-04-18 09:53:02 +0200

    information med json

---
+++ information_json.js
@@ -0,0 +1,51 @@
+document.addEventListener("DOMContentLoaded", start);
+
+let urlParameter = new URLSearchParams(window.location.search);
+
+let tekstId = urlParameter.get("id");
+
+
+
+/* ændre id'et til siden id */
+const startId = 309;
+
+
+let urlId;
+
+if (tekstId) {
+    urlId = tekstId;
+} else {
+    urlId = startId;
+}
+
+const url = "http://louisesettrup.dk/kea/09_cms/enogtyvefem_wp/wordpress/wp-json/wp/v2/tekst/" + urlId;
+
+
+let alleTekster;
+
+
+
+function start() {
+    hentData();
+
+}
+
+async function hentData() {
+    const response = await fetch(url);
+    alleTekster = await response.json();
+
+
+    /* ændre data navn til hver */
+    visDataInformation();
+}
+
+function visDataInformation() {
+    /*    document.querySelector(".splash_img").src = alleTekster.billede1.guid;*/
+    document.querySelector("#information_overskrift").textContent = alleTekster.title.rendered;
+    document.querySelector("#information_p1").innerHTML = alleTekster.content.rendered;
+
+    document.querySelector("#information_overskrift2").textContent = alleTekster.overskrift1;
+    document.querySelector("#information_p2").innerHTML = alleTekster.tekst1;
+
+
+}
