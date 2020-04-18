3efc7965cb55e7dcfd517fc9068e4947e4f19488
loui161h <55078332+loui161h@users.noreply.github.com>
2020-04-18 17:10:04 +0200

    udlejning blandt mellem grid og json done - mangler bookingssytem og hvordan kommer vi med

---
+++ booking_json.js
@@ -0,0 +1,58 @@
+document.addEventListener("DOMContentLoaded", start);
+
+let urlParameter = new URLSearchParams(window.location.search);
+
+let tekstId = urlParameter.get("id");
+
+
+
+/* ændre id'et til siden id */
+const startId = ;
+
+
+let urlId;
+
+if (tekstId) {
+    urlId = tekstId;
+} else {
+    urlId = startId;
+}
+
+const url = "http://louisesettrup.dk/kea/09_cms/enogtyvefem_wp/wordpress/wp-json/wp/v2/tekst/" + urlId;
+
+
+let alleTekster;
+
+
+
+function start() {
+    hentData();
+
+}
+
+async function hentData() {
+    const response = await fetch(url);
+    alleTekster = await response.json();
+
+
+    /* ændre data navn til hver */
+    visDataBooking();
+}
+
+function visDataBooking() {
+    /*    document.querySelector(".splash_img").src = alleTekster.billede1.guid;*/
+    document.querySelector("#").textContent = alleTekster.title.rendered;
+    document.querySelector("#").innerHTML = alleTekster.content.rendered;
+
+    document.querySelector("#").textContent = alleTekster.overskrift1;
+    document.querySelector("#").innerHTML = alleTekster.tekst1;
+
+    document.querySelector("#").textContent = alleTekster.overskrift2;
+    document.querySelector("#").innerHTML = alleTekster.tekst3;
+
+    document.querySelector("#").textContent = alleTekster.overskrift3;
+    document.querySelector("#").innerHTML = alleTekster.tekst5;
+
+    document.querySelector("#").textContent = alleTekster.overskrift4;
+    document.querySelector("#").innerHTML = alleTekster.tekst7;
+}
