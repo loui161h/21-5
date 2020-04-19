11647eace2c2dcc98f9e3bd3c370b315d38e17ec
loui161h <55078332+loui161h@users.noreply.github.com>
2020-04-19 14:26:39 +0200

    DK FAMILIER indhold og grid

---
+++ dk39_json.js
@@ -0,0 +1,57 @@
+document.addEventListener("DOMContentLoaded", start);
+
+let urlParameter = new URLSearchParams(window.location.search);
+
+let tekstId = urlParameter.get("id");
+
+
+
+/* ændre id'et til siden id */
+const startId = 305;
+
+
+let urlId;
+
+if (tekstId) {
+    urlId = tekstId;
+} else {
+    urlId = startId;
+}
+
+const url = "http://louisesettrup.dk/kea/09_cms/enogtyvefem_wp/wordpress/wp-json/wp/v2/tekst/" + urlId;
+
+
+let alleTekster;
+
+
+
+function start() {
+    hentData();
+
+}
+
+async function hentData() {
+    const response = await fetch(url);
+    alleTekster = await response.json();
+
+
+    /* ændre data navn til hver */
+    visDataDkFamily();
+}
+
+function visDataDkFamily() {
+    /*    document.querySelector(".splash_img").src = alleTekster.billede1.guid;*/
+    document.querySelector("#dk39_overskrift1").textContent = alleTekster.title.rendered;
+    document.querySelector("#dk39_p1").innerHTML = alleTekster.content.rendered;
+
+    document.querySelector("#dk39_overskrift2").textContent = alleTekster.overskrift1;
+    document.querySelector("#dk39_p2").innerHTML = alleTekster.tekst1;
+    document.querySelector("#dk39_p3").innerHTML = alleTekster.tekst2;
+
+    document.querySelector("#dk39_overskrift3").textContent = alleTekster.overskrift2;
+    document.querySelector("#dk39_p4").innerHTML = alleTekster.tekst3;
+
+
+    document.querySelector("#dk39_overskrift5").textContent = alleTekster.overskrift3;
+    document.querySelector("#dk39_overskrift6").textContent = alleTekster.overskrift4;
+}
