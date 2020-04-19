c84def6f4029abc0a1dd2e37699a3cc07d62b4cb
loui161h <55078332+loui161h@users.noreply.github.com>
2020-04-19 21:49:25 +0200

    ledige andele indhold

---
+++ ledigeandele_json.js
@@ -0,0 +1,50 @@
+document.addEventListener("DOMContentLoaded", start);
+
+let urlParameter = new URLSearchParams(window.location.search);
+
+let tekstId = urlParameter.get("id");
+
+
+
+/* ændre id'et til siden id */
+const startId = 308;
+
+
+let urlId;
+
+if (tekstId) {
+    urlId = tekstId;
+} else {
+    urlId = startId;
+}
+
+const url = "http://louisesettrup.dk/kea/09_cms/enogtyvefem_wp/wordpress/wp-json/wp/v2/tekst/" + urlId;
+
+
+let alleTekster;
+
+
+
+function start() {
+    hentData();
+
+}
+
+async function hentData() {
+    const response = await fetch(url);
+    alleTekster = await response.json();
+
+
+    /* ændre data navn til hver */
+    visDataLedigeAndele();
+}
+
+function visDataLedigeAndele() {
+    /*    document.querySelector(".splash_img").src = alleTekster.billede1.guid;*/
+    document.querySelector("#ledigeandele_overskrift1").textContent = alleTekster.title.rendered;
+    document.querySelector("#ledigeandele_p1").innerHTML = alleTekster.content.rendered;
+
+
+
+
+}
