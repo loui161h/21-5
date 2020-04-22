db4bb06690ad94f6110529543796a3b6405047cb
Cecilie Slemming <bruger@Brugers-MacBook-Pro.local>
2020-04-22 17:15:10 +0200

    booking

--- booking_json.js
+++ booking_json.js
@@ -41,36 +41,39 @@ async function hentData() {
 
 function visDataBooking() {
     document.querySelector("#bookingsystemet_splash_img").src = alleTekster.billede1.guid;
+
     document.querySelector("#bookingsystemet_overskrift1").textContent = alleTekster.title.rendered;
+     document.querySelector("#bookingsystemet_p1").innerHTML = alleTekster.content.rendered;
+
 
 
     document.querySelector("#bookingsystemet_overskrift2").textContent = alleTekster.overskrift1;
-    document.querySelector("#bookingsystemet_p1").textContent = alleTekster.tekst1;
+    document.querySelector("#bookingsystemet_p2").textContent = alleTekster.tekst1;
 
     document.querySelector("#bookingsystemet_overskrift3").innerHTML = alleTekster.overskrift2;
-    document.querySelector("#bookingsystemet_p2").innerHTML = alleTekster.tekst3;
+    document.querySelector("#bookingsystemet_p3").innerHTML = alleTekster.tekst3;
 
     document.querySelector("#bookingsystemet_overskrift4").innerHTML = alleTekster.overskrift3;
-    document.querySelector("#bookingsystemet_p3").innerHTML = alleTekster.tekst5;
+    document.querySelector("#bookingsystemet_p4").innerHTML = alleTekster.tekst5;
 
     document.querySelector("#bookingsystemet_overskrift5").innerHTML = alleTekster.overskrift4;
-    document.querySelector("#bookingsystemet_p4").innerHTML = alleTekster.tekst7;
+    document.querySelector("#bookingsystemet_p5").innerHTML = alleTekster.tekst7;
 
     document.querySelector("#bookingsystemet_overskrift6").innerHTML = alleTekster.overskrift5;
-    document.querySelector("#bookingsystemet_p5").innerHTML = alleTekster.tekst10;
+    document.querySelector("#bookingsystemet_p6").innerHTML = alleTekster.tekst10;
 
     document.querySelector("#bookingsystemet_overskrift7").innerHTML = alleTekster.overskrift6;
-    document.querySelector("#bookingsystemet_p6").innerHTML = alleTekster.tekst11;
+    document.querySelector("#bookingsystemet_p7").innerHTML = alleTekster.tekst11;
 
     document.querySelector("#booking_img1").src = alleTekster.billede2.guid;
 
     document.querySelector("#bookingsystemet_overskrift8").innerHTML = alleTekster.overskrift7;
-    document.querySelector("#bookingsystemet_p7").innerHTML = alleTekster.tekst12;
+    document.querySelector("#bookingsystemet_p8").innerHTML = alleTekster.tekst12;
 
     document.querySelector("#booking_img2").src = alleTekster.billede3.guid;
 
     document.querySelector("#bookingsystemet_overskrift9").innerHTML = alleTekster.overskrift8;
-    document.querySelector("#bookingsystemet_p8").innerHTML = alleTekster.tekst12;
+    document.querySelector("#bookingsystemet_p9").innerHTML = alleTekster.tekst12;
 
 
 
