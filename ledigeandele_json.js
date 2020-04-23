bb7b777e8ff538ad79e83a62de1c3a006a9fabdb
loui161h <55078332+loui161h@users.noreply.github.com>
2020-04-23 13:27:48 +0200

    ledige andele done

--- ledigeandele_json.js
+++ ledigeandele_json.js
@@ -43,6 +43,16 @@ function visDataLedigeAndele() {
     document.querySelector("#ledigeandele_splash_img").src = alleTekster.billede1.guid;
     document.querySelector("#ledigeandele_overskrift1").textContent = alleTekster.title.rendered;
     document.querySelector("#ledigeandele_p1").innerHTML = alleTekster.content.rendered;
+    document.querySelector("#ledige_img1").src = alleTekster.billede4.guid;
+    document.querySelector("#ledige_img2").src = alleTekster.billede2.guid;
+    document.querySelector("#ledige_img3").src = alleTekster.billede3.guid;
+    document.querySelector("#ledigeandele_overskrift2").textContent = alleTekster.overskrift1;
+    document.querySelector("#ledigeandele_p2").textContent = alleTekster.tekst1;
+    document.querySelector("#ledigeandele_p3").textContent = alleTekster.tekst2;
+    document.querySelector("#ledigeandele_overskrift3").textContent = alleTekster.overskrift2;
+    document.querySelector("#ledigeandele_p4").textContent = alleTekster.tekst3;
+    document.querySelector("#ledigeandele_p5").textContent = alleTekster.tekst4;
+    document.querySelector("#ledigeandele_overskrift4").textContent = alleTekster.overskrift3;
 
 
 
